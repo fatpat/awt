f18dac50565e839906d78e13f771f60b53a69c9e
Jérôme LOYET <822436+fatpat@users.noreply.github.com>
2018-10-24 07:05:48 +0200

    initial release for master acro

--- docs/assets/js/awt-rqsf.js
+++ docs/assets/js/awt-rqsf.js
@@ -6,7 +6,7 @@ function run_loop(interval) {
     Papa.parse(url, {
       download: true,
       complete: function(results, file) {
-        //console.log("Parsing complete:", results, file);
+//        console.log("Parsing complete:", results, file);
         var d = results.data;
 
         $('p.section').addClass('animated').addClass('bounce');
@@ -30,14 +30,8 @@ function run_loop(interval) {
         $( '#pilot-R3' ).text(d[6][6]);
         $( '#pilot-R4' ).text(d[7][6]);
 
-        // Group O
-        $( '#pilot-O1' ).text(d[4][7]);
-        $( '#pilot-O2' ).text(d[5][7]);
-        $( '#pilot-O3' ).text(d[6][7]);
-        $( '#pilot-O4' ).text(d[7][7]);
-
         // ** qualification runs **
-        for (var g=0; g<4; g++) {
+        for (var g=0; g<3; g++) {
           for (var i=0; i<6; i++) {
 
             var l = 'A';
@@ -56,49 +50,26 @@ function run_loop(interval) {
 
         // quarters
         for (var q=0; q<4; q++) {
-          var x = 53;
-          if (q == 1) x = 56;
-          if (q == 2) x = 59;
-          if (q == 3) x = 62;
+          var x = 69;
+          if (q == 1) x = 72;
+          if (q == 2) x = 75;
+          if (q == 3) x = 78;
           update_battle(final_runs["Q" + (q+1)], d[x][1], d[x][2], d[x+1][1], d[x+1][2]);
         }
 
-        // C1-C10
-        for (var i=0; i<10; i++) {
-          var x = 67;
-          var y = 4;
-          if (i == 1) { x=70; y=4; }
-          if (i == 2) { x=53; y=4; }
-          if (i == 3) { x=56; y=4; }
-          if (i == 4) { x=59; y=4; }
-          if (i == 5) { x=62; y=4; }
-          if (i == 6) { x=67; y=7; }
-          if (i == 7) { x=70; y=7; }
-          if (i == 8) { x=75; y=7; }
-          if (i == 9) { x=78; y=7; }
-          update_battle(final_runs["C" + (i+1)], d[x][y], d[x][y+1], d[x+1][y], d[x+1][y+1]);
-        }
-
-
         // S1-S2
         for (var i=0; i<2; i++) {
-          var x = 67;
-          var y = 1;
-          if (i == 1) { x=70; y=1; }
-          update_battle(final_runs["S" + (i+1)], d[x][y], d[x][y+1], d[x+1][y], d[x+1][y+1]);
-        }
-
-        // F1,F3,F5,F7,F9,F11,F13,F15
-        for (var i=0; i<8; i++) {
           var x = 83;
           var y = 1;
           if (i == 1) { x=86; y=1; }
-          if (i == 2) { x=89; y=1; }
-          if (i == 3) { x=92; y=1; }
-          if (i == 4) { x=83; y=4; }
-          if (i == 5) { x=86; y=4; }
-          if (i == 6) { x=89; y=4; }
-          if (i == 7) { x=92; y=4; }
+          update_battle(final_runs["S" + (i+1)], d[x][y], d[x][y+1], d[x+1][y], d[x+1][y+1]);
+        }
+
+        // F1,F3
+        for (var i=0; i<2; i++) {
+          var x = 99;
+          var y = 1;
+          if (i == 1) { x=102; y=1; }
           update_battle(final_runs["F" + (i*2 + 1)], d[x][y], d[x][y+1], d[x+1][y], d[x+1][y+1]);
         }
 
@@ -110,45 +81,35 @@ function run_loop(interval) {
         $( '#result-A4' ).text(d[49][3]);
 
         // Group C
-        $( '#result-C1' ).text(d[46][4]);
-        $( '#result-C2' ).text(d[47][4]);
-        $( '#result-C3' ).text(d[48][4]);
-        $( '#result-C4' ).text(d[49][4]);
+        $( '#result-C1' ).text(d[46][5]);
+        $( '#result-C2' ).text(d[47][5]);
+        $( '#result-C3' ).text(d[48][5]);
+        $( '#result-C4' ).text(d[49][5]);
 
         // Group R
-        $( '#result-R1' ).text(d[46][5]);
-        $( '#result-R2' ).text(d[47][5]);
-        $( '#result-R3' ).text(d[48][5]);
-        $( '#result-R4' ).text(d[49][5]);
-
-        // Group O
-        $( '#result-O1' ).text(d[46][6]);
-        $( '#result-O2' ).text(d[47][6]);
-        $( '#result-O3' ).text(d[48][6]);
-        $( '#result-O4' ).text(d[49][6]);
+        $( '#result-R1' ).text(d[46][7]);
+        $( '#result-R2' ).text(d[47][7]);
+        $( '#result-R3' ).text(d[48][7]);
+        $( '#result-R4' ).text(d[49][7]);
 
         // ** final runs **
 
         // ** final results **
-        $( '#result-p1' ).text(d[97][2]);
-        $( '#result-p2' ).text(d[98][2]);
-        $( '#result-p3' ).text(d[99][2]);
-        $( '#result-p4' ).text(d[100][2]);
-        $( '#result-p5' ).text(d[101][2]);
-        $( '#result-p6' ).text(d[102][2]);
-        $( '#result-p7' ).text(d[103][2]);
-        $( '#result-p8' ).text(d[104][2]);
-        $( '#result-p9' ).text(d[105][2]);
-        $( '#result-p10' ).text(d[106][2]);
-        $( '#result-p11' ).text(d[107][2]);
-        $( '#result-p12' ).text(d[108][2]);
-        $( '#result-p13' ).text(d[109][2]);
-        $( '#result-p14' ).text(d[110][2]);
-        $( '#result-p15' ).text(d[111][2]);
-        $( '#result-p16' ).text(d[112][2]);
+        $( '#result-p1' ).text(d[113][2]);
+        $( '#result-p2' ).text(d[114][2]);
+        $( '#result-p3' ).text(d[115][2]);
+        $( '#result-p4' ).text(d[116][2]);
+        $( '#result-p5' ).text(d[117][2]);
+        $( '#result-p6' ).text(d[118][2]);
+        $( '#result-p7' ).text(d[119][2]);
+        $( '#result-p8' ).text(d[120][2]);
+        $( '#result-p9' ).text(d[121][2]);
+        $( '#result-p10' ).text(d[122][2]);
+        $( '#result-p11' ).text(d[123][2]);
+        $( '#result-p12' ).text(d[124][2]);
 
         // refresh data in 5s
-        // run_loop(loop_interval);
+        run_loop(loop_interval);
       },
       error: function(error, file) {
         console.log("Parsing error:", error, file);
@@ -166,7 +127,7 @@ window.onload = function() {
   }
 
   // init SVG for '1st to 8th finals'
-  var finals = SVG('svg-finals-1-8').size(1360, 550).viewbox(0,0,1360,550);
+  var finals = SVG('svg-finals').size(1360, 550).viewbox(0,0,1360,550);
 
   // Create battle to reuse later
   var battle = create_battle(finals);
@@ -179,20 +140,16 @@ window.onload = function() {
 
   var F1 = battle_clone(battle, 'F1', (finals.width()-battle_width)/2, battle_sep_small, "final");
   var F3 = battle_clone(battle, 'F3', (finals.width()-battle_width)/2, battle_sep_small + battle_height + battle_sep_large, "final");
-  var F5 = battle_clone(battle, 'F5', (finals.width()-battle_width)/2, battle_sep_small + (battle_height + battle_sep_large)*2, "final");
-  var F7 = battle_clone(battle, 'F7', (finals.width()-battle_width)/2, battle_sep_small + (battle_height + battle_sep_large)*3, "final");
 
   var semi_pad = ((finals.width() - battle_width)/2 - battle_width)/2 - battle_width/2 + battle_sep_large;
   var S1 = battle_clone(battle, 'S1', battle_width + semi_pad, battle_sep_small + battle_height+battle_sep_large/2 -battle_height/2, "semi");
   var S2 = battle_clone(battle, 'S2', finals.width() - battle_width*2 - semi_pad, battle_sep_small + battle_height+battle_sep_large/2 -battle_height/2, "semi");
 
-  var C1 = battle_clone(battle, 'C1', battle_width + semi_pad, battle_sep_small + (battle_height+battle_sep_large)*3 - battle_sep_large/2 - battle_height/2, "semi");
-  var C2 = battle_clone(battle, 'C2', finals.width() - battle_width*2 - semi_pad, battle_sep_small + (battle_height+battle_sep_large)*3 - battle_sep_large/2 - battle_height/2, "semi");
 
   // update text
   text(Q1, 1, "1st Group A");
   text(Q1, 2, "2nd Group O");
-  Q1.addClass("quarter");
+  //Q1.addClass("quarter");
 
   text(Q2, 1, "2nd Group A");
   text(Q2, 2, "1st Group O");
@@ -209,23 +166,11 @@ window.onload = function() {
   text(S2, 1, "Winner Q2");
   text(S2, 2, "Winner Q3");
 
-  text(C1, 1, "Loser Q1");
-  text(C1, 2, "Loser Q4");
-
-  text(C2, 1, "Loser Q2");
-  text(C2, 2, "Loser Q3");
-
   text(F1, 1, "Winner S1");
   text(F1, 2, "Winner S2");
 
   text(F3, 1, "Loser S1");
   text(F3, 2, "Loser S2");
-
-  text(F5, 1, "Winner C1");
-  text(F5, 2, "Winner C2");
-
-  text(F7, 1, "Loser C1");
-  text(F7, 2, "Loser C2");
 
   // remove as it's not necessary anymore
   battle.remove();
@@ -242,79 +187,9 @@ window.onload = function() {
   conn(finals, S1, "bottom-right", F3, "left").addClass("loser");
   conn(finals, S2, "bottom-left", F3, "right").addClass("loser");
 
-  conn(finals, C1, "top-right", F5, "left").addClass("winner");
-  conn(finals, C2, "top-left", F5, "right").addClass("winner");
-  conn(finals, C1, "bottom-right", F7, "left").addClass("loser");
-  conn(finals, C2, "bottom-left", F7, "right").addClass("loser");
-
-  conn(finals, Q1, "left", C1, "left").addClass("loser");
-  conn(finals, Q4, "left", C1, "left").addClass("loser");
-  conn(finals, Q2, "right", C2, "right").addClass("loser");
-  conn(finals, Q3, "right", C2, "right").addClass("loser");
-
-
-  // clone div#svg-finals-1-8 into section#finals-9-16
-  $("#svg-finals-1-8").clone().appendTo("#finals-9-16");
-  // update IDs
-  $("#finals-9-16 > div").attr('id', "div-svg-finals-9-16");
-  $("#finals-9-16 > div > svg").attr('id', "svg-finals-9-16");
-
-  // got cloned SVG
-  finals2 = SVG.select('#svg-finals-9-16').first();
-
-  // rename battles
-  var C3  = rename_battle(finals2, "Q1", "C3");
-  var C4  = rename_battle(finals2, "Q2", "C4");
-  var C5  = rename_battle(finals2, "Q3", "C5");
-  var C6  = rename_battle(finals2, "Q4", "C6");
-  var C9  = rename_battle(finals2, "S1", "C9");
-  var C10 = rename_battle(finals2, "S2", "C10");
-  var C7  = rename_battle(finals2, "C1", "C7");
-  var C8  = rename_battle(finals2, "C2", "C8");
-  var F9  = rename_battle(finals2, "F1", "F9");
-  var F11 = rename_battle(finals2, "F3", "F11");
-  var F13 = rename_battle(finals2, "F5", "F13");
-  var F15 = rename_battle(finals2, "F7", "F15");
-
-  // update pilot's name
-  text(C3, 1, "3rd Group A");
-  text(C3, 2, "4th Group O");
-
-  text(C4, 1, "4th Group A");
-  text(C4, 2, "3rd Group O");
-
-  text(C5, 1, "3rd Group C");
-  text(C5, 2, "4th Group R");
-
-  text(C6, 1, "4th Group C");
-  text(C6, 2, "3rd Group R");
-
-  text(C7, 1, "Loser C3");
-  text(C7, 2, "Loser C6");
-
-  text(C8, 1, "Loser C4");
-  text(C8, 2, "Loser C5");
-
-  text(C9, 1, "Winner C3");
-  text(C9, 2, "Winner C6");
-
-  text(C10, 1, "Winner C4");
-  text(C10, 2, "Winner C5");
-
-  text(F9, 1, "Winner C9");
-  text(F9, 2, "Winner C10");
-
-  text(F11, 1, "Loser C9");
-  text(F11, 2, "Loser C10");
-
-  text(F13, 1, "Winner C7");
-  text(F13, 2, "Winner C8");
-
-  text(F15, 1, "Loser C7");
-  text(F15, 2, "Loser C8");
 
   // save each battle
-  final_runs = {C1:C1,C2:C2,C3:C3,C4:C4,C5:C5,C6:C6,C7:C7,C8:C8,C9:C9,C10:C10,Q1:Q1,Q2:Q2,Q3:Q3,Q4:Q4,S1:S1,S2:S2,F1:F1,F3:F3,F5:F5,F7:F7,F9:F9,F11:F11,F13:F13,F15:F15};
+  final_runs = {Q1:Q1,Q2:Q2,Q3:Q3,Q4:Q4,S1:S1,S2:S2,F1:F1,F3:F3};
 
 
   // Create qualification runs
@@ -339,7 +214,7 @@ window.onload = function() {
   var A6 = battle_clone(battle, 'A6', x, y);
 
   // group C
-  var x = battle_sep_small + battle_sep_large*2 + (battle_sep_large + battle_width);
+  var x = battle_sep_small + battle_sep_large*2 + (4*battle_sep_large + battle_width);
   var y = Y;
   var C1 = battle_clone(battle, 'C1', x, y);
   y += battle_height + battle_sep_small;
@@ -354,7 +229,7 @@ window.onload = function() {
   var C6 = battle_clone(battle, 'C6', x, y);
 
   // group R
-  var x = battle_sep_small + battle_sep_large*2 + (battle_sep_large + battle_width)*2;
+  var x = battle_sep_small + battle_sep_large*2 + (4*battle_sep_large + battle_width)*2;
   var y = Y;
   var R1 = battle_clone(battle, 'R1', x, y);
   y += battle_height + battle_sep_small;
@@ -368,21 +243,6 @@ window.onload = function() {
   y += battle_height + battle_sep_small;
   var R6 = battle_clone(battle, 'R6', x, y);
 
-  // group O
-  var x = battle_sep_small + battle_sep_large*2 + (battle_sep_large + battle_width)*3;
-  var y = Y;
-  var O1 = battle_clone(battle, 'O1', x, y);
-  y += battle_height + battle_sep_small;
-  var O2 = battle_clone(battle, 'O2', x, y);
-  y += battle_height + battle_sep_large;
-  var O3 = battle_clone(battle, 'O3', x, y);
-  y += battle_height + battle_sep_small;
-  var O4 = battle_clone(battle, 'O4', x, y);
-  y += battle_height + battle_sep_large;
-  var O5 = battle_clone(battle, 'O5', x, y);
-  y += battle_height + battle_sep_small;
-  var O6 = battle_clone(battle, 'O6', x, y);
-
   // remove useless template
   battle.remove();
 
@@ -390,14 +250,13 @@ window.onload = function() {
   qlf.text("A").move(A1.data('x') + battle_width/2, battle_sep_large).addClass('title').addClass('title_group');
   qlf.text("C").move(C1.data('x') + battle_width/2, battle_sep_large).addClass('title').addClass('title_group');
   qlf.text("R").move(R1.data('x') + battle_width/2, battle_sep_large).addClass('title').addClass('title_group');
-  qlf.text("O").move(O1.data('x') + battle_width/2, battle_sep_large).addClass('title').addClass('title_group');
 
   qlf.text("RUN 1").move(battle_sep_large + battle_sep_small, A2.data('y') - battle_sep_small).addClass('title').addClass('title_run').rotate(270);
   qlf.text("RUN 2").move(battle_sep_large + battle_sep_small, A4.data('y') - battle_sep_small).addClass('title').addClass('title_run').rotate(270);
   qlf.text("RUN 3").move(battle_sep_large + battle_sep_small, A6.data('y') - battle_sep_small).addClass('title').addClass('title_run').rotate(270);
 
   // save runs
-  qlf_runs = {A1:A1,A2:A2,A3:A3,A4:A4,A5:A5,A6:A6,C1:C1,C2:C2,C3:C3,C4:C4,C5:C5,C6:C6,R1:R1,R2:R2,R3:R3,R4:R4,R5:R5,R6:R6,O1:O1,O2:O2,O3:O3,O4:O4,O5:O5,O6:O6}
+  qlf_runs = {A1:A1,A2:A2,A3:A3,A4:A4,A5:A5,A6:A6,C1:C1,C2:C2,C3:C3,C4:C4,C5:C5,C6:C6,R1:R1,R2:R2,R3:R3,R4:R4,R5:R5,R6:R6}
 
   // run main loop that will fetch data and update the current page
   run_loop(0);
